68a4d294217041a2c94f186898e304d9e2449750
vaibhavscor <vaibhav.scor29@gmail.com>
2021-02-27 12:38:30 +0530

    test commit

--- app_main/forms.py
+++ app_main/forms.py
@@ -5,4 +5,7 @@ from django import forms
 class CreateNoteForm(forms.ModelForm):
 	class Meta:
 		model = Note
+		widgets = {
+            'Title': forms.TextInput(attrs={'class': 'myfieldclass'}),
+        }
 		fields = '__all__'
